8f7479102979f8a69b867431a22d8863001ba332
samdark <sam@rmcreative.ru>
2010-06-11 12:32:06 +0000

    documentation updates, release
    git-svn-id: http://yiiext.googlecode.com/svn/trunk/app/extensions/yiiext/renderers/smarty@223 175f29da-e50b-11de-93f0-cd01e99e34ab

--- ESmartyViewRenderer.php
+++ ESmartyViewRenderer.php
@@ -6,7 +6,7 @@
  * @link http://code.google.com/p/yiiext/
  * @link http://www.smarty.net/
  *
- * @version 0.9.6
+ * @version 0.9.7
  */
 class ESmartyViewRenderer extends CApplicationComponent implements IViewRenderer {
     public $fileExtension='.tpl';
@@ -27,27 +27,27 @@ class ESmartyViewRenderer extends CApplicationComponent implements IViewRenderer
         require_once('Smarty/Smarty.class.php');
 
         $this->smarty = new Smarty();
-        
+
         $this->smarty->template_dir = '';
         $compileDir = Yii::app()->getRuntimePath().'/smarty/compiled/';
-        
+
         // create compiled directory if not exists
         if(!file_exists($compileDir)){
             mkdir($compileDir, $this->filePermission, true);
         }
-                
+
         $this->smarty->compile_dir = $compileDir;
-        
-        
+
+
         $this->smarty->plugins_dir[] = Yii::getPathOfAlias('application.extensions.Smarty.plugins');
         if(!empty($this->pluginsDir)){
             $this->smarty->plugins_dir[] = Yii::getPathOfAlias($this->pluginsDir);
         }
-        
+
         if(!empty($this->configDir)){
             $this->smarty->config_dir = Yii::getPathOfAlias($this->configDir);
         }
-        
+
         $this->smarty->assign("TIME", sprintf('%0.5f',Yii::getLogger()->getExecutionTime()));
         $this->smarty->assign("MEMORY", round(Yii::getLogger()->getMemoryUsage()/(1024*1024),2)." MB");
         $this->smarty->assign('Yii', Yii::app());
@@ -69,10 +69,10 @@ class ESmartyViewRenderer extends CApplicationComponent implements IViewRenderer
         // check if view file exists
         if(!is_file($sourceFile) || ($file=realpath($sourceFile))===false)
             throw new CException(Yii::t('yiiext','View file "{file}" does not exist.', array('{file}'=>$sourceFile)));
-            
-        //assign data                
+
+        //assign data
 		$this->smarty->assign($data);
-        
+
         //render
         return $this->smarty->fetch($sourceFile);
 	}
