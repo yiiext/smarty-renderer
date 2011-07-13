422be0c6fceadfbfff30fca5b92488f890a243a5
CeBe <mail@cebe.cc>
2011-07-13 14:14:40 +0000

    ESmartyViewRenderer: corrected whitespace (the coding guidelines said 'intend with tabs' ;-) )
    git-svn-id: http://yiiext.googlecode.com/svn/trunk/app/extensions/yiiext/renderers/smarty@408 175f29da-e50b-11de-93f0-cd01e99e34ab

--- ESmartyViewRenderer.php
+++ ESmartyViewRenderer.php
@@ -9,56 +9,56 @@
  * @version 0.9.8
  */
 class ESmartyViewRenderer extends CApplicationComponent implements IViewRenderer {
-    public $fileExtension='.tpl';
-    public $filePermission=0755;
-    public $pluginsDir = null;
-    public $configDir = null;
+	public $fileExtension='.tpl';
+	public $filePermission=0755;
+	public $pluginsDir = null;
+	public $configDir = null;
 
-    private $smarty;
+	private $smarty;
 
-    /**
-     * Component initialization
-     */
-    function init(){
-        Yii::import('application.vendors.*');
+	/**
+	 * Component initialization
+	 */
+	public function init(){
+		Yii::import('application.vendors.*');
 
-        // need this since Yii autoload handler raises an error if class is not found
-        spl_autoload_unregister(array('YiiBase','autoload'));
+		// need this since Yii autoload handler raises an error if class is not found
+		spl_autoload_unregister(array('YiiBase','autoload'));
 
-        // including Smarty class and registering autoload handler
-        require_once('Smarty/Smarty.class.php');
+		// including Smarty class and registering autoload handler
+		require_once('Smarty/Smarty.class.php');
 
-        // adding back Yii autoload handler
-        spl_autoload_register(array('YiiBase','autoload'));
+		// adding back Yii autoload handler
+		spl_autoload_register(array('YiiBase','autoload'));
 
-        $this->smarty = new Smarty();
+		$this->smarty = new Smarty();
 
-        $this->smarty->template_dir = '';
-        $compileDir = Yii::app()->getRuntimePath().'/smarty/compiled/';
+		$this->smarty->template_dir = '';
+		$compileDir = Yii::app()->getRuntimePath().'/smarty/compiled/';
 
-        // create compiled directory if not exists
-        if(!file_exists($compileDir)){
-            mkdir($compileDir, $this->filePermission, true);
-        }
+		// create compiled directory if not exists
+		if(!file_exists($compileDir)){
+			mkdir($compileDir, $this->filePermission, true);
+		}
 
-        $this->smarty->compile_dir = $compileDir;
+		$this->smarty->compile_dir = $compileDir;
 
 
-        $this->smarty->plugins_dir[] = Yii::getPathOfAlias('application.extensions.Smarty.plugins');
-        if(!empty($this->pluginsDir)){
-            $this->smarty->plugins_dir[] = Yii::getPathOfAlias($this->pluginsDir);
-        }
+		$this->smarty->plugins_dir[] = Yii::getPathOfAlias('application.extensions.Smarty.plugins');
+		if(!empty($this->pluginsDir)){
+			$this->smarty->plugins_dir[] = Yii::getPathOfAlias($this->pluginsDir);
+		}
 
-        if(!empty($this->configDir)){
-            $this->smarty->config_dir = Yii::getPathOfAlias($this->configDir);
-        }
+		if(!empty($this->configDir)){
+			$this->smarty->config_dir = Yii::getPathOfAlias($this->configDir);
+		}
 
-        $this->smarty->assign("TIME", sprintf('%0.5f',Yii::getLogger()->getExecutionTime()));
-        $this->smarty->assign("MEMORY", round(Yii::getLogger()->getMemoryUsage()/(1024*1024),2)." MB");
-        $this->smarty->assign('Yii', Yii::app());
-    }
+		$this->smarty->assign("TIME", sprintf('%0.5f',Yii::getLogger()->getExecutionTime()));
+		$this->smarty->assign("MEMORY", round(Yii::getLogger()->getMemoryUsage()/(1024*1024),2)." MB");
+		$this->smarty->assign('Yii', Yii::app());
+	}
 
-    /**
+	/**
 	 * Renders a view file.
 	 * This method is required by {@link IViewRenderer}.
 	 * @param CBaseController the controller or widget who is rendering the view file.
@@ -68,19 +68,19 @@ class ESmartyViewRenderer extends CApplicationComponent implements IViewRenderer
 	 * @return mixed the rendering result, or null if the rendering result is not needed.
 	 */
 	public function renderFile($context,$sourceFile,$data,$return) {
-        // current controller properties will be accessible as {this.property}
-        $data['this'] = $context;
+		// current controller properties will be accessible as {this.property}
+		$data['this'] = $context;
 
-        // check if view file exists
-        if(!is_file($sourceFile) || ($file=realpath($sourceFile))===false)
-            throw new CException(Yii::t('yiiext','View file "{file}" does not exist.', array('{file}'=>$sourceFile)));
+		// check if view file exists
+		if(!is_file($sourceFile) || ($file=realpath($sourceFile))===false)
+			throw new CException(Yii::t('yiiext','View file "{file}" does not exist.', array('{file}'=>$sourceFile)));
 
-        //assign data
+		//assign data
 		$this->smarty->assign($data);
 
-        //render or return
+		//render or return
 		if($return)
-        	return $this->smarty->fetch($sourceFile);
+			return $this->smarty->fetch($sourceFile);
 		else
 			$this->smarty->display($sourceFile);
 	}
